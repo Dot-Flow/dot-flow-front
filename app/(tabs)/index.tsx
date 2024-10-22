5378d044516519eb4616e4313ea0ac632ddf69d9
Yejin <qja086351@naver.com>
2024-10-23 06:37:23 +0900

    메인 화면

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,171 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, Text, TextInput, View, TouchableOpacity,Dimensions, TouchableHighlight } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import {FullWindowOverlay} from 'react-native-screens';
+import {Camera, Paperclip} from 'lucide-react-native';
+import React from 'react';
+// import {Camera} from 'lucide-react-native';
+const { height } = Dimensions.get('window');
 
 export default function HomeScreen() {
+
+  var [ isPress, setIsPress ] = React.useState(false);
+
+  var touchProps = {
+    activeOpacity: 1,
+    underlayColor: 'blue',                               // <-- "backgroundColor" will be always overwritten by "underlayColor"
+    style: isPress ? styles.btnPress : styles.btnNormal, // <-- but you can still apply other style changes
+    onHideUnderlay: () => setIsPress(false),
+    onShowUnderlay: () => setIsPress(true),
+    onPress: () => console.log('HELLO'),                 // <-- "onPress" is apparently required
+  };
+  
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+    <ThemedView style={styles.container}>
+     <ThemedView style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="검색어를 입력해주세요"
+          placeholderTextColor="#ccc"
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
+
+      <ThemedView style={styles.goContainer}>
+        <ThemedView style={styles.iconsContainer}>
+        <TouchableOpacity>
+          <Camera color="#ccc" size={40} absoluteStrokeWidth={true} style={styles.icons}/>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Paperclip color="#ccc" size={35} absoluteStrokeWidth={true} style={styles.icons}/>
+        </TouchableOpacity>
+        </ThemedView>
+        
+
+        <TouchableOpacity
+          style={styles.goButton}
+          onPress={() => console.log('검색 → 글자')}>
+          <Text style={styles.goButtonText}>번역하기</Text>
+        </TouchableOpacity>
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
+
+      <ThemedView style={styles.buttonContainer}>
+      <TouchableOpacity 
+          style={styles.button}
+          onPress={() => console.log('점자 → 글자')}>
+          <Text style={styles.buttonText}>점자 → 글자</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => console.log('글자 → 점자')}>
+          <Text style={styles.buttonText}>글자 → 점자</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => console.log('한글 → 영어')}>
+          <Text style={styles.buttonText}>한글 → 영어</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => console.log('영어 → 한글')}>
+          <Text style={styles.buttonText}>영어 → 한글</Text>
+        </TouchableOpacity>
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+
+    </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
+  container: {
+    flex: 1,
+  },
+  inputContainer: {
+    marginTop: 30,
+    height: height * 2 / 5,
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  input: {
+    height: 50,
+    fontSize: 18,
+  },
+  goContainer:{
+    height: 50,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    flexDirection: 'row'
+  },
+  iconsContainer:{
+    justifyContent: 'flex-start',
     flexDirection: 'row',
     alignItems: 'center',
-    gap: 8,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  icons:{
+    marginRight: 10
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  goButton: {
+    backgroundColor: '#7359FF',
+    justifyContent: 'center',
+    borderRadius: 20,
+    alignItems: 'center',
+    height: 40,
+    width: 100,
   },
+  goButtonText:{
+    color: 'white',
+    fontSize: 16,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    justifyContent: 'space-between',
+  },
+  button: {
+    flexBasis: '48%',
+    backgroundColor: '#F7F5FF',
+    borderWidth: 1,
+    borderColor: '#7359FF',
+    justifyContent: 'center',
+    borderRadius: 4,
+    marginTop: 20,
+    marginBottom: 10,
+    alignItems: 'center',
+    height: 130
+  },
+  buttonText: {
+    color: '#7359FF',
+    fontSize: 16,
+  },
+  btnNormal: {
+    flexBasis: '48%',
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderColor: '#F7F5FF',
+    justifyContent: 'center',
+    borderRadius: 4,
+    marginTop: 20,
+    marginBottom: 10,
+    alignItems: 'center',
+    height: 130
+  },
+  btnPress: {
+    flexBasis: '48%',
+    backgroundColor: '#F7F5FF',
+    borderWidth: 1,
+    borderColor: '#7359FF',
+    justifyContent: 'center',
+    borderRadius: 4,
+    marginTop: 20,
+    marginBottom: 10,
+    alignItems: 'center',
+    height: 130
+  }
 });
