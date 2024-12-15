9d55a93a8fbfece40089863a4010396ea9f5e311
Yejin <qja086351@naver.com>
2024-12-15 17:56:05 +0900

    image to text api

--- services/translationApi.ts
+++ services/translationApi.ts
@@ -1,22 +1,23 @@
 import apiClient from "./apiClient";
+import {BrfResponse, TextResponse} from "./types";
 
 /**
  * Translate text string to braille
  * @param {string} text - The text to be brailed
- * @returns {Promise<string>} - Returns braille string
+ * @returns {BrfResponse} - Returns summary, unicode, file
  */
 const stringToBrf = (text: string) => {
 	const data = {
 		text,
 	};
 
-	return apiClient.post<string>(`/translate/to-brf/text`, data);
+	return apiClient.post<BrfResponse>(`/translate/to-brf/text`, data);
 };
 
 /**
  * Translate image file to brf
  * @param {string} fileUri - The image to be brailled
- * @returns {Promise<string>} - Returns braille string
+ * @returns {BrfResponse} -  Returns summary, unicode, file
  */
 const imageToBrf = (fileUri: string) => {
 	const formData = new FormData();
@@ -32,13 +33,37 @@ const imageToBrf = (fileUri: string) => {
 		type: type,
 	} as any);
 
-	return apiClient.post<Promise<string>>("translate/to-brf/image", formData, {
+	return apiClient.post<BrfResponse>("translate/to-brf/image", formData, {
 		headers: {
 			"Content-Type": "multipart/form-data",
 		},
 	});
-
-	// return apiClient.post<string>(`/translate/to-brf/text`, data);
 };
 
-export default {stringToBrf};
+/**
+ * Translate image file to text
+ * @param {string} fileUri - The image to be de-brailled
+ * @returns {TextResponse} - Returns summary, translation result, file
+ */
+const imageToText = (fileUri: string) => {
+	const formData = new FormData();
+	// Extract the file name and provide a fallback in case it's undefined
+	const filename = fileUri.split("/").pop() || "uploaded_file.jpg";
+
+	// Determine the MIME type based on the file extension
+	const type = `image/${filename.split(".").pop() || "jpeg"}`;
+
+	formData.append("multipartFile", {
+		uri: fileUri,
+		name: filename,
+		type: type,
+	} as any);
+
+	return apiClient.post<TextResponse>("translate/to-text/image", formData, {
+		headers: {
+			"Content-Type": "multipart/form-data",
+		},
+	});
+};
+
+export default {stringToBrf, imageToBrf, imageToText};
